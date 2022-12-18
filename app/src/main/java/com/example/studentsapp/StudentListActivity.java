27cb6a4edd57b1bd90948631ac407a7515ccc669
omer.damari <omer.damari@gmail.com>
2022-12-19 00:31:38 +0200

    feature/create_list_view

---
+++ app/src/main/java/com/example/studentsapp/StudentListActivity.java
@@ -0,0 +1,106 @@
+package com.example.studentsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.studentsapp.model.Model;
+import com.example.studentsapp.model.Student;
+
+import java.util.List;
+
+public class StudentListActivity extends AppCompatActivity {
+    List<Student> data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_list);
+
+        data = Model.instance().getAllStudents();
+
+        RecyclerView list = findViewById(R.id.studentlist_list);
+        list.setHasFixedSize(true);
+
+        list.setLayoutManager(new LinearLayoutManager(this));
+        StudentRecyclerAdapter adapter = new StudentRecyclerAdapter();
+        list.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(position -> {
+            Log.d("TAG", "row " + position + " was clicked");
+//            Intent intent = new Intent(this, AddStudentActivity.class);
+//            startActivity(intent);
+        });
+    }
+
+    class StudentViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTv;
+        TextView idTv;
+        CheckBox cb;
+
+        public StudentViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            nameTv = itemView.findViewById(R.id.studentlistrow_name_tv);
+            idTv = itemView.findViewById(R.id.studentlistrow_id_tv);
+            cb = itemView.findViewById(R.id.studentlistrow_cb);
+
+            cb.setOnClickListener(v -> {
+                int pos = (int)cb.getTag();
+                Student st = data.get(pos);
+                st.setCb(cb.isChecked());
+            });
+
+            itemView.setOnClickListener(v -> {
+                int pos = getAdapterPosition();
+                listener.onItemClick(pos);
+            });
+        }
+
+        public void bind(Student st, int position) {
+            nameTv.setText(st.getName());
+            idTv.setText(st.getId());
+            cb.setChecked(st.getCb());
+            cb.setTag(position);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    class StudentRecyclerAdapter extends RecyclerView.Adapter<StudentViewHolder> {
+        OnItemClickListener listener;
+        void setOnItemClickListener(OnItemClickListener listener) {
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.student_list_row, parent, false);
+
+
+            return new StudentViewHolder(view, listener);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+            Student st = data.get(position);
+            holder.bind(st, position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+    }
+}
